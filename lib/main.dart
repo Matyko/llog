762394d2bd3c69c18bc4334f149a8d501cdc9da9
Matyas Krista <matyas@geomiq.com>
2021-05-11 13:15:17 +0100

    Theme configurations

--- lib/main.dart
+++ lib/main.dart
@@ -39,101 +39,139 @@ class LLog extends StatelessWidget {
                       preference: settings.hasDarkMode,
                       builder: (_, hasDarkMode) {
                         return MaterialApp(
-                            theme: ThemeData(
-                                elevatedButtonTheme: ElevatedButtonThemeData(
-                                    style:
-                                    ElevatedButton.styleFrom(
-                                        primary: Colors.deepOrange)),
-                                primaryColor: Colors.deepOrange,
-                                primaryColorLight: Colors.deepOrange.shade100,
-                                primaryColorDark: Colors.deepOrange.shade700,
-                                accentColor: Colors.deepOrangeAccent,
-                                backgroundColor: hasDarkMode ? Colors.grey
-                                    .shade800 : Colors.white,
-                                scaffoldBackgroundColor: hasDarkMode ? Colors
-                                    .grey.shade900 : Colors.grey.shade200,
-                                cardColor: hasDarkMode
-                                    ? Colors.grey.shade700
-                                    : Colors.white,
-                                fontFamily: 'Montserrat',
-                                iconTheme: IconThemeData(
-                                    color: hasDarkMode ? Colors.white : Colors
-                                        .grey.shade800
-                                ),
-                                textTheme: TextTheme(
-                                  bodyText1: TextStyle(
-                                    color: hasDarkMode ? Colors.white : Colors
-                                        .grey.shade800,
-                                  ),
-                                  bodyText2: TextStyle(
-                                    color: hasDarkMode ? Colors.white : Colors
-                                        .grey.shade800,
-                                  ),
-                                  caption: TextStyle(
-                                    color: hasDarkMode ? Colors.white : Colors
-                                        .grey.shade800,
-                                  ),
-                                  headline1: TextStyle(
-                                    color: hasDarkMode ? Colors.white : Colors
-                                        .grey.shade800,
-                                  ),
-                                  headline2: TextStyle(
-                                    color: hasDarkMode ? Colors.white : Colors
-                                        .grey.shade800,
-                                  ),
-                                  subtitle1: TextStyle(
-                                    color: hasDarkMode ? Colors.white : Colors
-                                        .grey.shade800,
-                                  ),
-                                  subtitle2: TextStyle(
-                                    color: hasDarkMode ? Colors.white : Colors
-                                        .grey.shade800,
-                                  ),
-                                  headline3: TextStyle(
-                                    color: hasDarkMode ? Colors.white : Colors
-                                        .grey.shade800,
-                                  ),
-                                  headline4: TextStyle(
-                                    color: hasDarkMode ? Colors.white : Colors
-                                        .grey.shade800,
-                                  ),
-                                  headline5: TextStyle(
-                                    color: hasDarkMode ? Colors.white : Colors
-                                        .grey.shade800,
-                                  ),
-                                  headline6: TextStyle(
-                                    color: hasDarkMode ? Colors.white : Colors
-                                        .grey.shade800,
-                                  ),
-                                ),
-                                bottomNavigationBarTheme: BottomNavigationBarThemeData(
-                                  backgroundColor: hasDarkMode ? Colors.grey
-                                      .shade800 : Colors.white,
-                                  unselectedItemColor: hasDarkMode ? Colors
-                                      .white : Colors.grey.shade800,
-                                ),
-                                appBarTheme: AppBarTheme(
-                                    backgroundColor: hasDarkMode ? Colors.grey
-                                        .shade900 : Colors.grey.shade200,
-                                    shadowColor: Colors.transparent,
-                                    centerTitle: true,
-                                    textTheme: TextTheme(
-                                        headline6: TextStyle(
-                                            color: Colors.deepOrange,
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold,
-                                            fontFamily: 'PlayfairDisplay')),
-                                    iconTheme: IconThemeData(
-                                        color: Colors.deepOrange))),
-                            title: 'Quick Log',
+                            theme: _getThemeData(hasDarkMode),
+                            title: 'llog',
                             home: HomeScreen());
                       },
                     );
-                  }
-              );
-            }
-        ));
+                  });
+            }));
   }
+}
+
+ThemeData _getThemeData(bool hasDarkMode) {
+  return ThemeData(
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              primary: Colors.deepOrange)),
+      primaryColor: Colors.deepOrange,
+      primaryColorLight: Colors.deepOrange.shade100,
+      primaryColorDark: Colors.deepOrange.shade700,
+      accentColor: Colors.deepOrangeAccent,
+      backgroundColor: hasDarkMode
+          ? Colors.grey.shade800
+          : Colors.white,
+      scaffoldBackgroundColor: hasDarkMode
+          ? Colors.grey.shade900
+          : Colors.grey.shade200,
+      cardColor: hasDarkMode
+          ? Colors.grey.shade700
+          : Colors.white,
+      fontFamily: 'Montserrat',
+      iconTheme: IconThemeData(
+          color: hasDarkMode
+              ? Colors.white
+              : Colors.grey.shade800),
+      textTheme: TextTheme(
+        bodyText1: TextStyle(
+          color: hasDarkMode
+              ? Colors.white
+              : Colors.grey.shade800,
+        ),
+        bodyText2: TextStyle(
+          color: hasDarkMode
+              ? Colors.white
+              : Colors.grey.shade800,
+        ),
+        caption: TextStyle(
+          color: hasDarkMode
+              ? Colors.white
+              : Colors.grey.shade800,
+        ),
+        headline1: TextStyle(
+          color: hasDarkMode
+              ? Colors.white
+              : Colors.grey.shade800,
+        ),
+        headline2: TextStyle(
+          color: hasDarkMode
+              ? Colors.white
+              : Colors.grey.shade800,
+        ),
+        subtitle1: TextStyle(
+          color: hasDarkMode
+              ? Colors.white
+              : Colors.grey.shade800,
+        ),
+        subtitle2: TextStyle(
+          color: hasDarkMode
+              ? Colors.white
+              : Colors.grey.shade800,
+        ),
+        headline3: TextStyle(
+          color: hasDarkMode
+              ? Colors.white
+              : Colors.grey.shade800,
+        ),
+        headline4: TextStyle(
+          color: hasDarkMode
+              ? Colors.white
+              : Colors.grey.shade800,
+        ),
+        headline5: TextStyle(
+          color: hasDarkMode
+              ? Colors.white
+              : Colors.grey.shade800,
+        ),
+        headline6: TextStyle(
+          color: hasDarkMode
+              ? Colors.white
+              : Colors.grey.shade800,
+        ),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+          labelStyle: TextStyle(
+            color: hasDarkMode
+                ? Colors.white
+                : Colors.grey.shade800,
+          ),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+                color: hasDarkMode
+                    ? Colors.white
+                    : Colors.grey.shade800),
+          ),
+          hintStyle: TextStyle(
+            color: hasDarkMode
+                ? Colors.white
+                : Colors.grey.shade800,
+          )),
+      canvasColor: hasDarkMode
+          ? Colors.grey.shade800
+          : Colors.white,
+      bottomNavigationBarTheme:
+      BottomNavigationBarThemeData(
+        backgroundColor: hasDarkMode
+            ? Colors.grey.shade800
+            : Colors.white,
+        unselectedItemColor: hasDarkMode
+            ? Colors.white
+            : Colors.grey.shade800,
+      ),
+      appBarTheme: AppBarTheme(
+          backgroundColor: hasDarkMode
+              ? Colors.grey.shade900
+              : Colors.grey.shade200,
+          shadowColor: Colors.transparent,
+          centerTitle: true,
+          textTheme: TextTheme(
+              headline6: TextStyle(
+                  color: Colors.deepOrange,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'PlayfairDisplay')),
+          iconTheme: IconThemeData(
+              color: Colors.deepOrange)));
 }
 
 Future<bool> authUser(bool authRequired) async {
